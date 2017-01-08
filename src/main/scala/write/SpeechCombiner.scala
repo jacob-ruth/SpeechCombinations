f2e97b8d074a179c1e20bd44be242e4340866993
Jacob <jacob_ruth@brown.edu>
2017-01-07 23:04:15 -0500

    combinations with concat and temp files

--- src/main/scala/write/SpeechCombiner.scala
+++ src/main/scala/write/SpeechCombiner.scala
@@ -2,6 +2,9 @@ package write
 
 import com.github.tototoshi.csv._
 import java.io.File
+import java.io.PrintWriter
+import net.bramp.ffmpeg._
+import net.bramp.ffmpeg.builder._
 
 class SpeechCombiner(inputVidPath: String, inputTransPath: String, inputTextFilePath: String){
 
@@ -11,7 +14,7 @@ class SpeechCombiner(inputVidPath: String, inputTransPath: String, inputTextFile
 	var target: List[String] = List()
 	def loadTranscriptions() : Unit =  {
 
-		val targetwords = scala.io.Source.fromFile(inputTextFilePath).mkString
+		val targetwords = scala.io.Source.fromFile(inputTextFilePath).mkString.toLowerCase()
 		target = targetwords.split("[\\p{Punct}\\s]+").toList
 
 
@@ -28,9 +31,16 @@ class SpeechCombiner(inputVidPath: String, inputTransPath: String, inputTextFile
 				if(v.size != 4){
 					println("Problem! Skipping line")
 				}else{
-				    val loadedWord = new SpokenWord(v(0), v(1), v(2), v(3).toDouble, file.getName())
-
-				 	corpus = corpus + (v(0) -> loadedWord)
+					val fileName = inputVidPath + "/" + file.getName().substring(0, file.getName.indexOf('.')) + ".avi"
+				    val word = v(0).toLowerCase
+				    val loadedWord = new SpokenWord(word, v(1), v(2), v(3).toDouble, fileName)
+				    if(corpus.contains(word)){
+						if(corpus(word).conf < v(3).toDouble){
+							corpus = corpus + (word -> loadedWord)
+						}
+				    }else{
+				 	corpus = corpus + (word -> loadedWord)
+					}
 				}
 			}
 
@@ -56,7 +66,21 @@ class SpeechCombiner(inputVidPath: String, inputTransPath: String, inputTextFile
 	}
 
 	def createVideo(): Unit = {
-		println("We can make it!")
+		val pw = new PrintWriter(new File("temp", "combination.txt" ))
+		for(word <- target){
+			corpus(word).clipWord()
+			pw.write("file '" + word + ".mp4'\n")
+		}
+		pw.close()
+
+		val ffmpeg = new FFmpeg()
+		val ffprobe = new FFprobe()
+		val builder = new FFmpegBuilder()
+		val outName ="combination.mp4"
+		builder.addExtraArgs("-f", "concat").setInput("temp/combination.txt").overrideOutputFiles(true).addOutput(outName).done()
+		val executor = new FFmpegExecutor(ffmpeg, ffprobe); 
+		executor.createJob(builder).run();
+
 	}
 
 
--- src/main/scala/write/SpokenWord.scala
+++ src/main/scala/write/SpokenWord.scala
@@ -1,9 +1,21 @@
 package write
 
+import net.bramp.ffmpeg._
+import net.bramp.ffmpeg.builder._
+
 class SpokenWord(word: String, startTime: String, endTime: String,  confidence: Double, vidFile: String){
 
+	def conf = confidence
+	//creates a video clip for the chosen word
+	def clipWord(): Unit = {
+			val ffmpeg = new FFmpeg()
+			val ffprobe = new FFprobe()
+			val builder = new FFmpegBuilder()
+			val outName = word + ".mp4"
 
-
-
+			builder.setInput(vidFile).overrideOutputFiles(true).addOutput("temp/" + word +".mp4").addExtraArgs("-ss", startTime, "-t" , (endTime.toFloat - startTime.toFloat).toString).setVideoCodec("libx264") .setVideoFrameRate(24, 1).setVideoResolution(640, 480).done()
+			val executor = new FFmpegExecutor(ffmpeg, ffprobe); 
+			executor.createJob(builder).run();
+	}
 
 }
